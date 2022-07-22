78aa0b056985b47a9f47c281f07feaab40213d1a
Przemysław Buczkowski <prem@prem.moe>
2022-07-22 19:19:58 +0100

    Pull all platforms

--- build.sh
+++ build.sh
@@ -4,9 +4,15 @@ set -eux
 TAGS=(focal bionic jammy 18.04 20.04 22.04)
 PLATFORMS="linux/amd64,linux/arm64"
 
+IFS=',' read -ra PLATFORM_ARRAY <<< "$PLATFORMS"
+
 for tag in ${TAGS[@]}
 do
-    docker pull przemub/ubuntu-unminimized:$tag || true
+    for platform in ${PLATFORM_ARRAY[@]}
+    do
+        docker pull przemub/ubuntu-unminimized:$tag --platform $platform || true
+    done
+
     docker buildx build -f Dockerfile --platform $PLATFORMS -t przemub/ubuntu-unminimized:$tag --build-arg TAG=$tag --push .
 done
 
